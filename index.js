31d3fecb72a7627aa5ebff8ab67788f3665f8a13
Gal1h <turboch031@gmail.com>
2026-03-05 00:23:04 +0700

    commit

--- index.js
+++ index.js
@@ -50,13 +50,13 @@ client.on('message', async (msg) => {
     if (msg.hasMedia && msg.type === 'image') {
         try {
             const media = await msg.downloadMedia();
-            // if (message.body.toLowerCase().startsWith('.s'))
-            console.log(msg.body)
-            await client.sendMessage(msg.from, media, {
-                sendMediaAsSticker: true,
-                stickerName: "Sylvia Sticker Maker",
-                stickerAuthor: msg.author
-            });
+            if (message.body.toLowerCase().startsWith('.s')){
+                await client.sendMessage(msg.from, media, {
+                    sendMediaAsSticker: true,
+                    stickerName: "Sylvia Sticker Maker",
+                    stickerAuthor: msg.author
+                });
+            }
         } catch (err) {
             console.error("Gagal convert stiker:", err);
         }
